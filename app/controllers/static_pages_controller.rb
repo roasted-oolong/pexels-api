eaec22936704d7c5f6ecc30e269dfa65c1978269
Jenna Lee <65867683+roasted-oolong@users.noreply.github.com>
2025-12-03 06:07:40 +0000

    Display photos from a Pexels collection via unique ID
    - Controller fetches all photos from the collection using the Pexels gem
    - View displays the photos from the collection id submitted via form
    - Removed default h1 margin for cleaner spacing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
   def index
+    return unless params[:query].present?
+
+    pexels_api_key = Rails.application.credentials.pexels_api_key
+    client = Pexels::Client.new(pexels_api_key)
+
+    @photos = client.collections[params[:query]].media
   end
 end
